68683c1dac8fa50c0d4c7a5f85a2810c25376ddf
a-kv <aakvasovaa@gmail.com>
2020-06-16 10:03:09 +0300

    Updates

---
+++ precache-manifest.ad3b76e8adc5a65dd346261e1fe0d10f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7d568b0b281bbeee20a5abdd6d7d51",
+    "url": "/my-game/index.html"
+  },
+  {
+    "revision": "8e97a698ed5b86fe0dd9",
+    "url": "/my-game/static/css/main.c1d8cb64.chunk.css"
+  },
+  {
+    "revision": "4878ed60bd84de89a3eb",
+    "url": "/my-game/static/js/2.20fe6ecd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-game/static/js/2.20fe6ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e97a698ed5b86fe0dd9",
+    "url": "/my-game/static/js/main.00b126b6.chunk.js"
+  },
+  {
+    "revision": "af84f6413bc45294e0fb",
+    "url": "/my-game/static/js/runtime-main.7836311e.js"
+  },
+  {
+    "revision": "9a80272f15c4763540bf82a1c89b7e62",
+    "url": "/my-game/static/media/logo.9a80272f.gif"
+  },
+  {
+    "revision": "ebd22e58783e825042e19f19699488d5",
+    "url": "/my-game/static/media/mic.ebd22e58.png"
+  },
+  {
+    "revision": "6f7f9879571d6d150bbfe5744d1db69b",
+    "url": "/my-game/static/media/piz.6f7f9879.cur"
+  },
+  {
+    "revision": "fc3397bebceba6fffaf813149ec23aa6",
+    "url": "/my-game/static/media/prikolnoe-chavkane.fc3397be.mp3"
+  }
+]);
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore} from "redux";
-import {reducer} from "./reduser";
-
-
-const store = createStore(reducer)
-export default store;
